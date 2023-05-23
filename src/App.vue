0c95a941ed84e2057590ba4ffd34323b51fae713
pauljsyi <pauljsyi@gmail.com>
2023-05-23 11:31:48 -0600

    added rank feature and styling

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,6 @@ export default {
       this.delay = 2000 + Math.random() * 5000;
       this.isPlaying = true;
       this.showResults = false;
-      console.log(this.delay);
     },
     endGame(reactionTime) {
       this.score = reactionTime;
@@ -44,5 +43,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
